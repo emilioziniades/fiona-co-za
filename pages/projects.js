719ad2145f9c4329e9b94942f2a95960660072dc
Emilio Ziniades <emilioziniades@protonmail.com>
2022-06-20 15:17:38 +0200

    start on projects page
    - removed welcome component
    - added utility functions to query data
    - laid out images in flex 1/1/1
    - added hover transition

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,28 @@
+import Image from "next/image";
 import Layout from "../components/layout";
-import Welcome from "../components/welcome";
-export default function ProjectsPage() {
+import { getSortedProjectsData } from "../utils/markdown";
+
+export async function getStaticProps() {
+  const allProjectsData = getSortedProjectsData();
+  return {
+    props: {
+      allProjectsData,
+    },
+  };
+}
+
+export default function ProjectsPage({ allProjectsData }) {
+  console.log(allProjectsData);
   return (
     <Layout>
-      <Welcome path="/projects" />
+      <div className="flex ">
+        {allProjectsData.map((project) => (
+          <div className="basis-1/3 m-1 relative ">
+            <Image src={project.image} height={300} width={300} />
+            <div className="transition-all hover:bg-stone-600 h-full w-full opacity-50 absolute top-0 left-0" />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 }
